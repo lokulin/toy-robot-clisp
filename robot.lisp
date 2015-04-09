6154bc4926687dd77e0432715b326dc06a3b6e52
lokulin <lauchlin@lauchlin.com>
2015-04-09 20:38:41 +1000

    Create more bugs

--- robot.lisp
+++ robot.lisp
@@ -5,40 +5,40 @@
 
 ;;Robot functions
 
-(defun move-robot (r) 
-  (if (on-table (in-front-of r)) 
-    (in-front-of r)
-    r
-    ))
+(defun move-robot (r) (if (placed? r) (forward r) r))
 
-(defun report-robot (r)
-  (with-slots (x y facing) r
-    (format t "~d,~d,~a~%" x y facing)
-    r))
+(defun report-robot (r lookup)
+  (if (placed? r) (with-slots (x y facing) r 
+    (format t "~d,~d,~f,~a~%" (round x) (round y) facing (lookup facing)) r )))
 
 (defun place-robot (r x y facing &optional (table (robot-table r)))
-  (make-robot :x x :y y :facing facing :table table))
+  (if (on-table? x y table)
+      (make-robot :x x :y y :facing facing :table table)
+      r))
 
-(defun left-robot (r)
-  (turn-robot r #'+))
-
-(defun right-robot (r)
-  (turn-robot r #'-))
+(defun left-robot (r) (turn-robot r #'toleft))
+(defun right-robot (r) (turn-robot r #'toright))
 
 ;;Helper functions
 
 (defun turn-robot (r fn)
-  (with-slots (x y facing rspeed) r
-    (place-robot r x y (mod (funcall fn facing rspeed) 2))))
+  (if (placed? r) (with-slots (x y facing rspeed) r
+    (place-robot r x y (mod (funcall fn facing rspeed) 2))) r ))
 
-(defun on-table (r)
-  (with-slots (x y table) r
-    (with-slots (llx lly urx ury) table
-      (and table (<= llx x)(<= lly y)(>= urx x)(>= ury y)))))
-
-(defun in-front-of (r)
+(defun forward (r)
   (with-slots (x y facing fspeed) r
     (place-robot r (+ x (* fspeed (cos (* pi facing))))
             (+ y (* fspeed (sin (* pi facing))))
             facing)))
+
+(defun on-table? (x y table)
+    (with-slots (llx lly urx ury) table
+      (and table (<= llx x)(<= lly y)(>= urx x)(>= ury y))))
+
+(setf (symbol-function `toleft) (symbol-function `+))
+(setf (symbol-function `toright) (symbol-function `-))
+
+(defun placed? (r)
+  (with-slots (x y facing) r
+    (and x y facing)))
 
--- toyrobot.lisp
+++ toyrobot.lisp
@@ -1,42 +1,59 @@
 ;;;; toyrobot.lisp
 
+;;; Toy Robot simulation runner. Reads and validates commands and sends them to the robot.
+
 (in-package #:toyrobot)
 
-;; Robot and Table structs
+;; Table structs
 (defstruct table llx lly urx ury)
 
-(setf *commands* "MOVELEFTRIGHTREPORTPLACE")
-(defparameter *dirs* (make-hash-table :size 4))
-(setf (gethash "EAST" *dirs*) '0)
-(setf (gethash "NORTH" *dirs*) '0.5)
-(setf (gethash "WEST" *dirs*) '1)
-(setf (gethash "SOUTH" *dirs*) '1.5)
+;; Input validation stuff
+(defun validcommand? (command) 
+    (search command "MOVELEFTRIGHTREPORTPLACE"))
 
-;; Input, validation main loop
+(defun finddirection (direction)
+  (case (intern direction :toyrobot)
+    ('EAST 0)
+    ('NORTH 0.5)
+    ('WEST 1)
+    ('SOUTH 1.5)
+    (otherwise nil)))
+
+(defun lookup (direction)
+  (case direction 
+    (0.0 "EAST")
+    (0.5 "NORTH")
+    (1.0 "WEST")
+    (1.5 "SOUTH")
+    (otherwise nil)))
+
+;; place has arguments so split them out and send them
 (defun place (r args)
-  (let ((direction (gethash (intern (subseq args (+ (search "," args :from-end t) 1))) *dirs*)))
+  (let ((direction (finddirection (subseq args (+ (search "," args :from-end t) 1)))))
     (if (not (null direction))
       (place-robot r 1 1 direction)
       r)))
 
+;; other commands don't have any arguments
 (defun move (r args)
   (move-robot r))
 
 (defun left (r args)
-  (right-robot r))
-
-(defun right (r args)
   (left-robot r))
 
-(defun report (r args)
-  (report-robot r))
+(defun right (r args)
+  (right-robot r))
 
-(defun run-toyrobot ()
+(defun report (r args)
+  (report-robot r #'lookup)
+  r)
+
+(defun run-toyrobot (file)
   (let ((thetable (make-table :llx 0 :lly 0 :urx 4 :ury 4)))
-  (let ((therobot (make-robot :x 0 :y 0 :facing 0 :table thetable)))
-  (with-open-file (input "examples/example6.txt")
-    (loop for line = (read-line input nil)
+    (let ((therobot (make-robot :table thetable)))
+    (with-open-file (input file)
+      (loop for line = (read-line input nil)
           while line do
           (let ((command (subseq line 0 (search " " line))))
             (let ((arguments (if (equal command line) () (subseq line (search " " line)))))
-              (if (search command *commands*) (setf therobot (funcall (intern command :toyrobot) therobot arguments))))))))))
+              (if (validcommand? command) (setf therobot (funcall (intern command :toyrobot) therobot arguments))))))))))
